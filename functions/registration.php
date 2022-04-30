3fb72467212c0c0549e6cc9e12f6b25a76cab28e
daniil <danchik.kun@mail.ru>
2022-04-30 10:31:10 +0300

    Сделал регистрацию

---
+++ functions/registration.php
@@ -0,0 +1,39 @@
+<?php
+    require './connect.php';
+
+    $full_name = $_POST['full_name'];
+    $login = $_POST['login'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $password_confirm = $_POST['password_confirm'];
+
+    if ($password === $password_confirm){
+        $avatar = 'avatars/' . time() . $_FILES['avatar']['name'];
+        move_uploaded_file($_FILES['avatar']['tmp_name'], '../' . $avatar);
+
+        $check_login = $connect->query("SELECT * FROM `users` WHERE `login` = '$login'");
+        $response = $check_login->fetch(PDO::FETCH_ASSOC);
+        if ($response['login']){
+            $_SESSION['message'] = "Этот логин занят, придумайте другой!";
+            header("Location: ../output/index.php");
+        }
+        else{
+            $check_email = $connect->prepare("SELECT * FROM `users` WHERE `email` = '$email'");
+            $check_email->execute();
+            $response1 = $check_email->fetch(PDO::FETCH_ASSOC);
+            if($response1['email']){
+                $_SESSION['message'] = "Такая почта уже используется, введите свою почту!";
+                header('Location: ../output/registration.php'); 
+            }
+            else{
+                $add_user = $connect->prepare("INSERT INTO `users`(`id`, `login`, `email`, `avatar`, `password`) VALUES (NULL,'$login','$email','$avatar','$password')");
+                $add_user->execute();
+                $_SESSION['message'] = "Регистрация прошла успешно!";
+                header('Location: ../output/index.php');
+            }
+        }
+    }
+    else{
+        $_SESSION['message'] = 'Пароли не совпадают!';
+        header('Location: ../output/index.php'); 
+    }
---
+++ output/index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Главная</title>
+</head>
+<body class="bg-dark">
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+            <div class="container">
+              <a class="navbar-brand" href="#">
+                  <img src="../images/logo.png" class="img-fluid" style="width: 200px;" alt="">
+                </a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Переключатель навигации">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Главная</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Каталог</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">О нас</a>
+                  </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2 search" type="search" placeholder="Поиск" aria-label="Поиск">
+                    <button class="search-logo" type="submit"><img src="../images/search.png" alt=""></button>
+                </form>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModalToggle">
+                    Войти
+                </button>
+              </div>
+            </div>
+          </nav>
+    </header>
+
+    <!-- Модальное окно -->
+    <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content bg-dark">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalToggleLabel">Войти в аккаунт</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form>  <!---------------------------------------------------------------------------------------------------------->
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">Ваш email</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">Пароль</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <button type="submit" class="btn btn-primary">Войти</button>
+              </form> <!---------------------------------------------------------------------------------------------------------->
+            </div>
+            <div class="modal-footer">
+              <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" data-bs-dismiss="modal">Нет аккаунта? Зарегистрируйтесь!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content bg-dark">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalToggleLabel2">Регистрация</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form method="post" enctype="multipart/form-data" action="../functions/registration.php"> 
+                <!---------------------------------------------------------------------------------------------------------->
+                <div class="row mb-3">
+                  <label for="inputEmail3" class="col-sm-2 col-form-label">ФИО</label>
+                  <div class="col-sm-10">
+                    <input type="text" name="full_name" class="form-control" id="inputNamel3">
+                  </div>
+                </div>
+                <div class="row mb-3">
+                  <label for="inputEmail3" class="col-sm-2 col-form-label">Логин</label>
+                  <div class="col-sm-10">
+                    <input type="text" name="login" class="form-control" id="inputNamel3">
+                  </div>
+                </div>
+                <div class="row mb-3">
+                  <label for="inputEmail3" class="col-sm-2 col-form-label">Почта</label>
+                  <div class="col-sm-10">
+                    <input type="email" name="email" class="form-control" id="inputEmaill3">
+                  </div>
+                </div>
+                <div class="mb-3">
+                  <label for="formFile" class="form-label">Выберите аватар</label>
+                  <input class="form-control" name="avatar" type="file" id="formFile">
+                </div>
+                <div class="row mb-3">
+                  <label for="inputPassword3" class="col-sm-2 col-form-label">Пароль</label>
+                  <div class="col-sm-10">
+                    <input type="password" name="password" class="form-control" id="inputPassword3">
+                  </div>
+                </div>
+                <div class="row mb-3">
+                  <label for="inputPassword3" class="col-sm-2 col-form-label">Повторите пароль</label>
+                  <div class="col-sm-10">
+                    <input type="password" name="password_confirm" class="form-control" id="inputPassword3">
+                  </div>
+                </div>
+                <button type="submit" class="btn btn-primary btn-sing-in-2">Зарегистрироваться</button>
+                <?php 
+                        if ($_SESSION['message']){
+                            echo '<p class="message">' . $_SESSION['message'] . '</p>';
+                        }
+                        unset($_SESSION['message']);
+                    ?> 
+              </form>
+              <!---------------------------------------------------------------------------------------------------------->
+            </div>
+            <div class="modal-footer">
+              <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal" data-bs-dismiss="modal">Уже есть аккаунт? Войдите в него!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <section class="section1">
+        <div class="container sec1_div">
+          <div class="row">
+            <div class="col">
+              <img class="img-fluid" src="../images/img-sec-1 1.png" alt="">
+            </div>
+            <div class="col">
+              <h1>Кафе "Теснота"</h1>
+              <p class="lead">В тесноте, да не в обиде</p>
+              <a class="btn btn-primary" href="#" role="button">Перейти к каталогу →</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
