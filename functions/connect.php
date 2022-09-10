7caa22fbc534a4503a91a2f715674e1588bb005a
Daniil Kunitcyn <danchik.kun@mail.ru>
2022-09-10 13:35:18 +0300

    доделал редактирование меню

--- functions/connect.php
+++ functions/connect.php
@@ -2,7 +2,7 @@
     $host = 'localhost';
     $dbname = 'tesnota';
     $username = 'root';
-    $password = 'root';
+    $password = '';
 
     try{
         $connect = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
--- functions/edit_menu_done.php
+++ functions/edit_menu_done.php
@@ -3,14 +3,21 @@ session_start();
 
 require './connect.php';
 
-echo $name = $_POST['name'];
-echo $discription = $_POST['discription'];
-echo $price = $_POST['price'];
-echo $type = $_POST['type'];
-echo $id = $_POST['id'];
+$name = $_POST['name'];
+$discription = $_POST['discription'];
+$price = $_POST['price'];
+$type = $_POST['type'];
+$id = $_POST['id'];
+
 
 $img = 'img_menu/' . $_FILES['img']['name'];
-move_uploaded_file($_FILES['img']['tmp_name'], '../' . $img);
 
-// $sql = $connect->query("INSERT INTO `menu` (`name`, `img`, `discription`, `price`, `type`) VALUES ('$name', '$img', '$discription', '$price', '$type') WHERE `id` = '$id'");
-// header("Location: ../output/menu");
+if($img == 'img_menu/'){
+
+}else{
+    move_uploaded_file($_FILES['img']['tmp_name'], '../' . $img);
+    $add_img = $connect->query("UPDATE `menu` SET `img` = '$img' WHERE `menu`.`id` = '$id'");
+}
+
+$sql = $connect->query("UPDATE `menu` SET `name` = '$name', `discription` = '$discription', `price` = '$price', `type` = '$type' WHERE `menu`.`id` = '$id'");
+header("Location: ../output/menu");
